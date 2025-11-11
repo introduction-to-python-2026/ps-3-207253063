84dbe361db1cfd3421148c6d7d0b25409ab64e2d
207253063 <gal.zuk@live.biu.ac.il>
2025-11-11 10:52:49 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 def approximate_pi(n_terms):
   approximate_pi = 0
   for i in range(n_terms):
-    approximate_pi += 4 * ((-1) ** i / (2 * i + 1))
-  return approximate_pi 
+    approximate_pi += 4 * ((-1)**i / (2*i +1))
+  return approximate_pi
